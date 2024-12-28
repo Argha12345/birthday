a2c509aa829a289fb673bc1b411ed50cfb7e8d7b
Argha12345 <149236102+Argha12345@users.noreply.github.com>
2024-12-28 12:02:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("surpriseButton").addEventListener("click", function() {
+    const surprise = document.getElementById("surprise");
+    surprise.classList.remove("hidden");
+  });
+  
+  
